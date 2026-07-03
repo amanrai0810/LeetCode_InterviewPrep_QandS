aa35195e0e335f7d02ab0207e6c9cf99765e1763
Aman Rai <138314040+amanrai0810@users.noreply.github.com>
2026-07-03 15:19:43 +0530

    Time: 33 ms (73.89%), Space: 59.1 MB (73.93%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,30 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(nums);  
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for(int i = 0; i < nums.length - 2; i++){
+        Arrays.sort(nums);
 
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            // Duplicate first element skip karo
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
 
             int left = i + 1;
             int right = nums.length - 1;
 
-            while(left < right){
+            while (left < right) {
+
                 int sum = nums[i] + nums[left] + nums[right];
 
-                if(sum == 0){
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    // skip duplicates
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    while(left < right && nums[right] == nums[right - 1]) right--;
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
 
                     left++;
                     right--;
 
-                } else if(sum < 0){
+                    // Left duplicate skip
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Right duplicate skip
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+
+                } else if (sum < 0) {
                     left++;
                 } else {
                     right--;
@@ -32,6 +43,6 @@ class Solution {
             }
         }
 
-        return result;
+        return ans;
     }
 }
